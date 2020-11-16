1e683e2bdf50972289a478a891755181193bb73b
Luke Wu <luke.wu@hancock.pro>
2020-11-16 17:47:20 +0800

    Response yml file when user push send button

--- Index/index.js
+++ Index/index.js
@@ -1,3 +1,14 @@
 document.getElementById('sendBtn').onclick = function() {
-    console.log(123);
+    
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            document.body.textContent = request.responseText;
+            console.log(123);
+            console.log(request.responseText);
+        }
+    }
+
+    request.open("POST", "http://localhost:8000/yml", true)
+    request.send(null)
 };
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var yaml = require('js-yaml');
+var fs = require('fs');
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
@@ -13,6 +15,20 @@ app.get('/index.css', function(req, res) {
 
 app.get('/index.js', function(req, res) {
     res.sendFile(path.join(__dirname + '/Index/index.js'));
+});
+
+app.post('/yml', function(req, res) {
+    try {
+        let fileContents = fs.readFileSync(
+            './yml/Lutu-TF.yml',
+            'utf8'
+        );
+        let data = yaml.safeLoad(fileContents);
+        console.log(data);
+        res.send(data);
+    } catch(e) {
+        console.log(e);
+    }
 });
 
 app.listen(8000, function() {
