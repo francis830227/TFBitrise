6faa50481883258477c402e7015bc93dbbc3b9bb
Luke Wu <luke.wu@hancock.pro>
2020-11-17 17:19:12 +0800

    Complete Functionality

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 
+//Regular Express Rule
+var teamIdRules = /^[A-Za-z0-9]*$/;
+var buildRules = /^[0-9]*$/;
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/Index/index.html'));
+    console.log(teamIdRules.test("1.13"))
 });
 
 app.get('/index.css', function(req, res) {
@@ -23,6 +27,18 @@ app.post('/yml', function(req, res) {
 
     console.log(req.body);
     
+    var body = req.body;
+
+    if (!(teamIdRules.test(body.teamId))) {
+        res.send("TeamId Invalid.")
+        return
+    }
+
+    if (!(buildRules.test(body.build))) {
+        res.send("Build Invalid.")
+        return
+    }
+
     try {
         var fileContents = fs.readFileSync(
             './yml/Lutu-TF.yml',
@@ -30,9 +46,27 @@ app.post('/yml', function(req, res) {
         );
         var data = yaml.safeLoad(fileContents);
         
-        console.log(data.workflows[5]);//.set-ios-product-bundle-identifier@1.inputs.new_bundle_identifier);
-        
-        data.workflows[5] = {"123": "4465"};
+        //Bundle Identifier
+        data.workflows["Lutu-Beta"].steps[5]['set-ios-product-bundle-identifier@1']["inputs"][0]["new_bundle_identifier"] = body.bundleId;
+    
+        //Bundle version
+        data.workflows["Lutu-Beta"].steps[6]['set-ios-version@2']["inputs"][0]["bundle_version"] = body.version;
+
+        //Bundle version short
+        data.workflows["Lutu-Beta"].steps[6]['set-ios-version@2']["inputs"][1]["bundle_version_short"] = body.build;
+
+        //Debug Provision-Profile Specifier
+        data.workflows["Lutu-Beta"].steps[7]['code-signing-setting-patch@1']["inputs"][2]["debug_provisioning_profile_specifier"] = body.profileName;
+
+        //Release Provision-Profile Specifier
+        data.workflows["Lutu-Beta"].steps[7]['code-signing-setting-patch@1']["inputs"][4]["release_provisioning_profile_specifier"] = body.profileName;
+
+        //Debug development team
+        data.workflows["Lutu-Beta"].steps[7]['code-signing-setting-patch@1']["inputs"][6]["debug_development_team"] = body.teamId;
+
+        //Release development team
+        data.workflows["Lutu-Beta"].steps[7]['code-signing-setting-patch@1']["inputs"][7]["release_development_team"] = body.teamId;
+
         res.send(data);
     } catch(e) {
         console.log(e);
